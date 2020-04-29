7ca98f874a5b17e1f86a815f8bff9335854885a6
YUJO42 <bedro27@gmail.com>
2020-04-29 17:07:31 +0900

    frontend Done

--- quiz_app_test/lib/screen/screen_quiz.dart
+++ quiz_app_test/lib/screen/screen_quiz.dart
@@ -1,6 +1,9 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:quizapptest/model/model_quiz.dart';
+import 'package:quizapptest/screen/screen_result.dart';
+import 'package:quizapptest/widget/widget_candidate.dart';
 
 class QuizScreen extends StatefulWidget {
   List<Quiz> quizs;
@@ -13,12 +16,15 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
   List<int> _answers = [-1, -1, -1];
-  List<bool> _answerState = [false, false, false];
+  List<bool> _answerState = [false, false, false, false];
   int _currentIndex = 0;
+  SwiperController _controller = SwiperController();
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
+    Size screenSize = MediaQuery
+        .of(context)
+        .size;
     double width = screenSize.width;
     double height = screenSize.height;
     return SafeArea(
@@ -33,6 +39,7 @@ class _QuizScreenState extends State<QuizScreen> {
             width: width * 0.85,
             height: height * 0.5,
             child: Swiper(
+              controller: _controller,
               physics: NeverScrollableScrollPhysics(),
               loop: false,
               itemCount: widget.quizs.length,
@@ -66,8 +73,97 @@ class _QuizScreenState extends State<QuizScreen> {
               ),
             ),
           ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                fontSize: width * 0.048,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            children: _buildCandidates(width, quiz),
+          ),
+          Container(
+            padding: EdgeInsets.all(width * 0.024),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.5,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: RaisedButton(
+                  child: _currentIndex == widget.quizs.length - 1
+                      ? Text('결과보기')
+                      : Text('다음문제'),
+                  textColor: Colors.white,
+                  color: Colors.deepPurple,
+                  onPressed: _answers[_currentIndex] == -1
+                      ? null
+                      : () {
+                    if (_currentIndex == widget.quizs.length - 1) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ResultScreen(
+                                answers: _answers,
+                                quizs: widget.quizs,
+                              ),
+                          ),
+                      );
+                    } else {
+                      _answerState = [false, false, false, false];
+                      _currentIndex += 1;
+                      _controller.next();
+                    }
+                  },
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+    for (int i = 0; i < 4; i++) {
+      _children.add(
+        CandWidget(
+          index: i,
+          text: quiz.candidates[i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                } else {
+                  _answerState[j] = false;
+                }
+              }
+            });
+          },
+        ),
+      );
+      _children.add(
+        Padding(
+          padding: EdgeInsets.all(width * 0.024),
+        ),
+      );
+    }
+    return _children;
   }
 }
