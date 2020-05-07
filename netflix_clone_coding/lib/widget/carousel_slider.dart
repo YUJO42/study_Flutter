20bb78cf54a58d5ca0b7929202dccdd9b81ab2b5
yujo <yujo@student.42seoul.kr>
2020-05-07 21:37:01 +0900

    study flutter

---
+++ netflix_clone_coding/lib/widget/carousel_slider.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:netflixclonecoding/model/model_movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+  CarouselImage({this.movies});
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  List<Movie> movies;
+  List<Widget> images;
+  List<String> keywords;
+  List<bool> likes;
+  int _currentPage = 0;
+  String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
